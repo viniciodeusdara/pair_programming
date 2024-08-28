2b374a4742bc2bc458d84f94eb2e636ff06ae696
Vinicio Deusdara <viniciodeusdara@gmail.com>
2024-08-28 07:33:56 -0300

    Exercício resolvido

--- testes.py
+++ testes.py
@@ -1,27 +1,27 @@
-# [{"nome": "Alice", "notas": [8.5, 9.0, 7.5], "curso": "Matemática"},{"nome": "Bob", "notas": [6.0, 5.5, 7.0], "curso": "Física"},] -> Caso "Normal"
-# [{"notas": [8.5, 9.0, 7.5], "curso": "Matemática"}]
-# [{"nome": "Alice", "curso": "Matemática"}]
-# [{"nome": "Alice", "notas": [8.5, 9.0, 7.5]}]
-# [{"nome": 123, "notas": [8.5, 9.0, 7.5], "curso": "Matemática"}]
-# [{"nome": 123, "notas": [8.5, 9.0, 7.5], "curso": "Matemática"}]
-# Caso com nenhum argumento
-# Caso aluno aprovado
-# Caso aluno reprovado
-# Caso números negativos
-# Caso lista em ordem não decrescente
-def media(notas):
-    return sum(notas) / len(notas)
-
-def aluno_aprovado(aluno, nota_minima=7.0):
-    return media(aluno['notas']) >= nota_minima
-    
 alunos = [
     {"nome": "Alice", "notas": [8.5, 9.0, 7.5], "curso": "Matemática"},
     {"nome": "Bob", "notas": [6.0, 5.5, 7.0], "curso": "Física"},
     {"nome": "Charlie", "notas": [9.5, 8.0, 9.0], "curso": "Computação"}
 ]
 
-alunos_aprovados = list(filter(aluno_aprovado, alunos))
-            
-print(alunos_aprovados)
+
+def media(aluno):
+    aluno['media'] = round(sum(aluno['notas']) / len(aluno['notas']), 2)
+    return aluno
+
+def is_aprovado(aluno):
+    return aluno['media'] >= 7
+       
+
+def extrair_media(aluno):
+    return aluno['media']
+
+
+aprovados = []
+for aluno in filter(is_aprovado, list(map(media, alunos))):
+    aprovados.append(aluno)
+
+print(sorted(aprovados, key=extrair_media, reverse=True))
+
+
 
